e32bcc985c0d36a9e7018e95bfa4ddce72542acc
roland.pakai <roland.pakai@lightware.com>
2023-10-27 19:40:47 +0200

    feat: use bodyParser.json()

--- src/express/index.ts
+++ src/express/index.ts
@@ -1,13 +1,17 @@
 import compression from 'compression'
 import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
 
 import express, { Application } from 'express'
 import helmet from 'helmet'
 import morgan from 'morgan'
 
+import api from '../api/index.ts'
+
 const createServer = (): Application => {
   const app = express()
 
+  app.use(bodyParser.json())
   app.use(compression())
   app.use(express.urlencoded({ extended: true }))
   app.use(express.json())
@@ -15,6 +19,8 @@ const createServer = (): Application => {
   app.use(helmet())
   app.use(cookieParser())
   app.use(morgan('dev'))
+
+  app.use('/api', api())
 
   return app
 }
